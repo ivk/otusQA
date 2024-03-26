090ef7d9eaea49b00d109dc9bdefa409a17b8877
ivk <i_smith@mail.ru>
2024-03-26 22:04:20 +0300

    markers added

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -15,13 +15,23 @@ from triangle import Triangle
 EPSILON = 0.05
 
 
+@pytest.fixture(scope="session", autouse=True)
+def run_stop_tests():
+    print('\nTests started')
+
+    yield
+
+    print('\nTests finished')
+
+
+@pytest.mark.positive
 @pytest.mark.parametrize(("figure", "sides", "expected_area"), [
     (Triangle, (3, 4, 5), 6),
     (Rectangle, (3, 4), 12),
     (Square, (3,), 9),
     (Circle, (1,), math.pi)
 ])
-def _test_figure_area_positive(figure, sides, expected_area):
+def test_figure_area_positive(figure, sides, expected_area):
     # print(f"{figure.__name__} checking...")
     f = figure(*sides)
 
@@ -30,14 +40,14 @@ def _test_figure_area_positive(figure, sides, expected_area):
     else:
         assert expected_area == f.get_area()
 
-
+@pytest.mark.positive
 @pytest.mark.parametrize(("figure", "sides", "expected_perimeter"), [
     (Triangle, (3, 4, 5), 12),
     (Rectangle, (3, 4), 14),
     (Square, (3,), 12),
     (Circle, (2,), 4*math.pi)
 ])
-def _test_figure_perimeter_positive(figure, sides, expected_perimeter):
+def test_figure_perimeter_positive(figure, sides, expected_perimeter):
     f = figure(*sides)
 
     if isinstance(expected_perimeter, float):
@@ -45,7 +55,7 @@ def _test_figure_perimeter_positive(figure, sides, expected_perimeter):
     else:
         assert expected_perimeter == f.get_perimeter()
 
-
+@pytest.mark.negative
 @pytest.mark.parametrize(("figure", "sides"), [
     (Triangle, (1, 1, 100)),
     (Rectangle,  (-3, 4)),
